b46064feb0506ddd50dd5d11e688a9f4205be4af
Vishwajeet Yadav <vishwajeety14122@gmail.com>
2023-11-20 15:41:38 +0530

    Update dependencies and configuration files

--- next.config.js
+++ next.config.js
@@ -1,11 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ["lh3.google.com", "lh3.googleusercontent.com", "img.freepik.com"], // Add other domains if needed
+    // domains: ["lh3.google.com", "lh3.googleusercontent.com", "img.freepik.com"], // Add other domains if needed
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+      {
+        protocol: "http",
+        hostname: "**",
+      },
+    ],
   },
   webpack(config) {
     config.experiments = {
