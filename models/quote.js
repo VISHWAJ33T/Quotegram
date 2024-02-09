1a2bf7d400406e904f2cc292289806391538334a
VISHWAJ33T <vishwajeety14122@gmail.com>
2024-02-09 22:27:13 +0530

    Remove duplicate creator field in QuoteSchema

--- models/quote.js
+++ models/quote.js
@@ -1,7 +1,7 @@
 import mongoose, { Schema, model, models } from "mongoose";
 
 const QuoteSchema = new Schema({
-  creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  creator: { type: mongoose.Schema.Types.ObjectId },
   // creator: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
   quote: { type: String, required: [true, "Quote is Required"] },
   author: { type: String, required: [true, "Author is Required"] },
